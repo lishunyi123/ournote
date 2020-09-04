4370739a11da34fca30617c39e98a5e1d7ef83d5
李顺仪 <lishunyi0109@gmail.com>
2020-09-04 18:02:43 +0800

    :tada: 初始提交！

---
+++ ournote-gateway/src/main/java/com/lishunyi/ournote/gateway/OurnoteGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lishunyi.ournote.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurnoteGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurnoteGatewayApplication.class, args);
+    }
+
+}
---
+++ ournote-gateway/src/test/java/com/lishunyi/ournote/gateway/OurnoteGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lishunyi.ournote.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurnoteGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
